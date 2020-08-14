8e7a02e296743eaba47e5659678faacedd12e641
Hong Tang <hong.tang@gmail.com>
2020-08-14 14:17:57 -0700

    Added top-down constrain-based search and cut down exploration space significantly (6 number search from 3 seconds to 30ms). Also removed duplicates for a-b and b-a (0), or a/b and b/a (1).

--- Find24/find24.cpp
+++ Find24/find24.cpp
@@ -18,12 +18,16 @@ void Find24::run(bool debug) {
     buildSolutionMap();
     
     if (debug) {
-        std::cout << "counters: " <<
-        "subsets=" << counters_.subsets <<
-        ", combos=" << counters_.combos <<
-        ", valcombos=" << counters_.valcombos <<
-        ", exprcombos=" << counters_.exprcombos <<
-        ", uniqexprs=" << counters_.uniqexprs << std::endl;
+        std::cout << "counters: " << std::endl <<
+        "subsets=" << counters_.subsets << std::endl <<
+        "combos=" << counters_.combos << std::endl <<
+        "newvalues=" << counters_.newvalues << std::endl <<
+        "valcombos=" << counters_.valcombos << std::endl <<
+        "exprcombos=" << counters_.exprcombos << std::endl <<
+        "uniqexprs=" << counters_.uniqexprs << std::endl <<
+        "csubsets=" << counters_.csubsets << std::endl <<
+        "ccombos=" << counters_.ccombos << std::endl <<
+        "cvalcombos=" << counters_.cvalcombos << std::endl;
     }
 }
 
@@ -62,6 +66,12 @@ void Find24::addLiterals()
             ++counters_.subsets;
         }
     }
+}
+
+void Find24::addRootConstraint() {
+    ValSet value;
+    value.insert(Rational(target_));
+    constraint_.insert(make_pair(elems_, value));
 }
 
 template<typename Op>
@@ -108,9 +118,11 @@ static void splitVec(const NumVec& from, int sel[], int k, NumVec& s1, NumVec& s
 
 class Find24::ValueBuilder {
 public:
-    ValueBuilder(const NumVec& key, ValExprMap& value, SolutionMap& solution,
-                 Counters& counters)
-    : key_(key), value_(value), solution_(solution), counters_(counters) { }
+    ValueBuilder(const NumVec& key, ValExprMap& value,
+                 const SolutionMap& solution,
+                 const ValSet* constraint, Counters& counters)
+    : key_(key), value_(value), solution_(solution), constraint_(constraint), counters_(counters) { }
+    
     void operator() (int* sel, int k) {
         NumVec s1, s2;
         splitVec(key_, sel, k, s1, s2);
@@ -135,12 +147,20 @@ private:
     const NumVec& key_;
     ValExprMap& value_;
     const SolutionMap& solution_;
+    const ValSet* constraint_;
     Counters& counters_;
     
     void doPlus(const ValExprMap::value_type& left, const ValExprMap::value_type& right)
     {
         Rational result=left.first + right.first;
-        ExprSet& exprs=value_[result];
+        if ( (constraint_) && (constraint_->find(result)==constraint_->end()) )
+            return;
+        auto it = value_.find(result);
+        if (it == value_.end()) {
+            ++counters_.newvalues;
+            it=value_.insert(make_pair(result, ExprSet())).first;
+        }
+        ExprSet& exprs=it->second;
         for (auto& lexpr : left.second) {
             for (auto& rexpr : right.second) {
                 ++counters_.exprcombos;
@@ -156,12 +176,22 @@ private:
     void doMinus(const ValExprMap::value_type& left, const ValExprMap::value_type& right)
     {
         Rational result=left.first - right.first;
-        if (result < 0) return;
+        if (result < Rational(0)) return;
+
+        if ( (constraint_) && (constraint_->find(result)==constraint_->end()) )
+            return;
         
-        ExprSet& exprs=value_[result];
+        auto it = value_.find(result);
+        if (it == value_.end()) {
+            ++counters_.newvalues;
+            it=value_.insert(make_pair(result, ExprSet())).first;
+        }
+        ExprSet& exprs=it->second;
+        bool checkZero = result==Rational(0);
         for (auto& lexpr : left.second) {
             for (auto& rexpr : right.second) {
                 ++counters_.exprcombos;
+                if (checkZero && cmpExpr(lexpr, rexpr)>0) continue;
                 std::unique_ptr<Expr> expr(new AddSub(lexpr, rexpr, true));
                 if (exprs.find(expr.get()) == exprs.end()) {
                     ++counters_.uniqexprs;
@@ -174,7 +204,15 @@ private:
     void doMultiple(const ValExprMap::value_type& left, const ValExprMap::value_type& right)
     {
         Rational result=left.first * right.first;
-        ExprSet& exprs=value_[result];
+        if ( (constraint_) && (constraint_->find(result)==constraint_->end()) )
+            return;
+        
+        auto it = value_.find(result);
+        if (it == value_.end()) {
+            ++counters_.newvalues;
+            it=value_.insert(make_pair(result, ExprSet())).first;
+        }
+        ExprSet& exprs=it->second;
         for (auto& lexpr : left.second) {
             for (auto& rexpr : right.second) {
                 ++counters_.exprcombos;
@@ -189,12 +227,22 @@ private:
     
     void doDivision(const ValExprMap::value_type& left, const ValExprMap::value_type& right)
     {
-        if (right.first == 0) return;
+        if (right.first == Rational(0)) return;
         Rational result=left.first / right.first;
-        ExprSet& exprs=value_[result];
+        if ( (constraint_) && (constraint_->find(result)==constraint_->end()) )
+            return;
+        
+        auto it = value_.find(result);
+        if (it == value_.end()) {
+            ++counters_.newvalues;
+            it=value_.insert(make_pair(result, ExprSet())).first;
+        }
+        ExprSet& exprs=it->second;
+        bool checkOne=result==Rational(1);
         for (auto& lexpr : left.second) {
             for (auto& rexpr : right.second) {
                 ++counters_.exprcombos;
+                if (checkOne && cmpExpr(lexpr, rexpr)>0) continue;
                 std::unique_ptr<Expr> expr(new MulDiv(lexpr, rexpr, true));
                 if (exprs.find(expr.get()) == exprs.end()) {
                     ++counters_.uniqexprs;
@@ -207,32 +255,152 @@ private:
 
 class Find24::SolutionBuilder {
 public:
-    SolutionBuilder(Find24* parent) : p_(parent) { }
+    SolutionBuilder(Find24& parent, bool check_constraint) :
+    p_(parent), check_constraint_(check_constraint) { }
+    
     void operator() (int* sel, int k) {
         NumVec key;
         for (int i=0; i<k; ++i) {
-            key.push_back(p_->elems_[sel[i]]);
+            key.push_back(p_.elems_[sel[i]]);
         }
-        if (p_->solution_.find(key) == p_->solution_.end()) {
+        if (p_.solution_.find(key) == p_.solution_.end()) {
             ValExprMap value;
-            ValueBuilder vb(key, value, p_->solution_, p_->counters_);
+            ValSet* constraint=0;
+            if (check_constraint_) {
+                auto it=p_.constraint_.find(key);
+                assert(it != p_.constraint_.end());
+                constraint=&(it->second);
+            }
+            ValueBuilder vb(key, value, p_.solution_, constraint, p_.counters_);
             for (int i=1; i<=key.size()/2; ++i) {
                 selectK((int)key.size(), i, vb);
             }
-            p_->solution_.insert(make_pair(key, value));
-            ++p_->counters_.subsets;
+            p_.solution_.insert(make_pair(key, value));
+            ++p_.counters_.subsets;
         }
     }
     
 private:
-    Find24* p_;
+    Find24& p_;
+    bool check_constraint_;
+};
+
+class Find24::CVBuilder {
+public:
+    CVBuilder(const NumVec& ckey, const NumVec& eelems, ValSet& value, const ConstraintMap& constraint, const SolutionMap& solution, Counters& counters) :
+    ckey_(ckey), eelems_(eelems), value_(value), constraint_(constraint), solution_(solution), counters_(counters) { }
+    
+    void operator() (int* sel, int k) {
+        // we are trying to find the constraint for one of the operands (ckey) of the
+        // following equations: sum = ckey op other and sum = other op ckey, with the
+        // knowledge of the constraints for sum and the possible values of others.
+        NumVec sum, other;
+        int ck_index=0;
+        for (int i=0; i<k; ++i) {
+            other.push_back(eelems_[sel[i]]);
+            while ( (ck_index < ckey_.size()) && (ckey_[ck_index]<=eelems_[sel[i]]) ) {
+                sum.push_back(ckey_[ck_index]);
+                ++ck_index;
+            }
+            sum.push_back(eelems_[sel[i]]);
+        }
+
+        while (ck_index < ckey_.size()) {
+            sum.push_back(ckey_[ck_index]);
+            ++ck_index;
+        }
+
+        const ValSet& sum_constraint=(constraint_.find(sum))->second;
+        const ValExprMap& other_values=(solution_.find(other))->second;
+        
+        // neither sum_constraint nor right_values should be empty
+        for (auto& i : sum_constraint) {
+            for (auto& j : other_values) {
+                ++counters_.cvalcombos;
+                doMinus(i, j.first); // i = x + j
+                doPlus(i, j.first); // i = x - j
+                doMinus(j.first, i); // i = j - x
+                doDivision(i, j.first); // i = x*j
+                doMultiple(i, j.first); // i = x/j
+                doDivision(j.first, i); // i = j/x
+            }
+        }
+        ++counters_.ccombos;
+    }
+    
+private:
+    const NumVec& ckey_;
+    const NumVec& eelems_;
+    ValSet& value_;
+    const ConstraintMap& constraint_;
+    const SolutionMap& solution_;
+    Counters& counters_;
+    
+    void doPlus(const Rational& left, const Rational& right)
+    {
+        Rational result=left + right;
+        value_.insert(result);
+    }
+    
+    void doMinus(const Rational& left, const Rational& right)
+    {
+        Rational result=left - right;
+        if (result < 0) return;
+        value_.insert(result);
+    }
+    
+    void doMultiple(const Rational& left, const Rational& right)
+    {
+        Rational result=left * right;
+        value_.insert(result);
+    }
+    
+    void doDivision(const Rational& left, const Rational& right)
+    {
+        if (right == 0) return;
+        Rational result=left / right;
+        value_.insert(result);
+    }
+};
+
+class Find24::ConstraintBuilder {
+public:
+    ConstraintBuilder(Find24& p) : p_(p) {}
+    void operator () (int* sel, int k) {
+        NumVec ckey; // key to the constraint map
+        NumVec eelems; // expanding elems
+        splitVec(p_.elems_, sel, k, eelems, ckey);
+        if (p_.constraint_.find(ckey) == p_.constraint_.end()) {
+            ValSet value;
+            CVBuilder cvb(ckey, eelems, value, p_.constraint_, p_.solution_, p_.counters_);
+            for (int i=1; i<=(int)eelems.size(); ++i) {
+                selectK((int)eelems.size(), i, cvb);
+            }
+            p_.constraint_.insert(make_pair(ckey, value));
+            ++p_.counters_.csubsets;
+        }
+    }
+    
+private:
+    Find24& p_;
 };
 
 void Find24::buildSolutionMap() {
     addLiterals();
-    SolutionBuilder sb(this);
-    for (int i=2; i<=elems_.size(); ++i) {
+    SolutionBuilder sb(*this, false);
+    for (int i=2; i<=elems_.size()/2; ++i) {
         selectK((int)elems_.size(), i, sb);
+    }
+    
+    addRootConstraint();
+    ConstraintBuilder cb(*this);
+    for (int i=1; i<=elems_.size()/2; ++i) {
+        selectK((int)elems_.size(), i, cb);
+    }
+    
+    SolutionBuilder sb2(*this, true);
+    for (int i=(int)elems_.size()/2+1; i<=elems_.size(); ++i) {
+        selectK((int)elems_.size(), i, sb2);
     }
 }
 
--- Find24/find24.hpp
+++ Find24/find24.hpp
@@ -21,6 +21,8 @@ typedef std::vector<int> NumVec;
 typedef std::set<Expr*, ExprCmp> ExprSet;
 typedef std::map<Rational, ExprSet> ValExprMap;
 typedef std::map<NumVec, ValExprMap> SolutionMap;
+typedef std::set<Rational> ValSet;
+typedef std::map<NumVec, ValSet> ConstraintMap;
 
 // Although the name comes from the game find-24, this class is a general
 // solution that can find arithmatic expressions that would yield a specific
@@ -48,19 +50,27 @@ private:
     int target_;
     NumVec elems_;
     SolutionMap solution_;
+    ConstraintMap constraint_;
     
     struct Counters {
         int subsets;
         int combos;
+        int newvalues;
         int valcombos;
         int exprcombos;
         int uniqexprs;
-        Counters() : subsets(0), combos(0), valcombos(0), exprcombos(0), uniqexprs(0) { }
+        int csubsets;
+        int ccombos;
+        int cvalcombos;
+        Counters() : subsets(0), combos(0), newvalues(0), valcombos(0), exprcombos(0), uniqexprs(0), csubsets(0), ccombos(0), cvalcombos(0) { }
     } counters_;
     
     void addLiterals();
+    void addRootConstraint();
     class ValueBuilder;
     class SolutionBuilder;
+    class CVBuilder;
+    class ConstraintBuilder;
     void buildSolutionMap();
     void freeSolutionMap();
 };
